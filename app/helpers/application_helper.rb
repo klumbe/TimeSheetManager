ea0855e8c6d6592b6ab5856388306980ae8bbf7e
Christian Müller <2605803+klumbe@users.noreply.github.com>
2018-05-10 22:06:49 +0200

    Added span to active navbar item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,24 @@
 module ApplicationHelper
 
+
   def current_class(current_path)
-    if request.path == current_path
+    if is_path_active?(current_path)
       return 'active'
     else
       return ''
     end
   end
+
+  def add_span(current_path)
+    if is_path_active?(current_path)
+      return "<span class=\"sr-only\">(current)</span>"
+    else
+      return ''
+    end
+  end
+
+  private
+    def is_path_active?(current_path)
+      return request.path == current_path
+    end
 end
