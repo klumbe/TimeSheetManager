cead37f00e6f8c0f5dd102bba3bd87ea7d04b278
Christian Müller <2605803+klumbe@users.noreply.github.com>
2018-12-01 21:42:29 +0100

    Added basic validation to TimeEntry model

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,11 +1,34 @@
 class TimeEntry < ApplicationRecord
   belongs_to :user
 
+  validate :end_time_cannot_be_lower_than_start_time, :total_cannot_be_negative
+
   def total
-    return Time.zone.at(self[:end] - self[:start] - (self[:breaks].hour * 3600 + self[:breaks].min * 60))
+    if total_minutes < 0
+      return Time.zone.at(0)
+    else
+      return Time.zone.at(total_minutes)
+    end
+  end
+
+  def total_minutes
+    return self[:end] - self[:start] - (self[:breaks].hour * 3600 + self[:breaks].min * 60)
   end
 
   def total=(total)
+  end
+
+  def end_time_cannot_be_lower_than_start_time
+    if self[:end] < self[:start]
+      errors.add(:end, "can't be before the start")
+      errors.add(:start, "can't be after the end")
+    end
+  end
+
+  def total_cannot_be_negative
+    if total_minutes < 0
+      errors.add(:total, "can't be negative")
+    end
   end
 
   def self.round_time(time)
