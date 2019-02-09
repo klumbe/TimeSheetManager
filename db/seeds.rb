79d71d59d65513ab947f74b30645193152a2f67c
Christian Müller <2605803+klumbe@users.noreply.github.com>
2019-02-09 15:44:03 +0100

    Added seeds for the dev database and changed the init-script to apply the seeds, too

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+
+  # create test-user
+  password = "TestPW234!"
+  user = User.create!(
+                       {
+                          email: "test@test.net",
+                          password: password,
+                          password_confirmation: password,
+                       }
+                     )
+
+  start_year = 2000
+  end_year = 2019
+  (start_year..end_year).each do |year|
+      (1..12).each do |month|
+        now = Time.current
+        starttime = Time.new(now.year, now.month, now.day, 9, 0, 0)
+        endtime = starttime.advance(hours: 8, minutes: 45)
+        (1..Time.days_in_month(month, year)).each do |day|
+            TimeEntry.create!({
+                                date: Date.new(year, month, day),
+                                start: starttime,
+                                end: endtime,
+                                breaks: Time.new(now.year, now.month, now.day, 0, 45, 0),
+                                user_id: user.id,
+                            })
+          end
+      end
+  end
+
+
+
+end
--- init.sh
+++ init.sh
@@ -4,9 +4,5 @@
 docker-compose up -d
 
 # create DB
-echo "Creating the database..."
-docker-compose exec -d web /bin/bash -c "cd /app && rake db:create"
-
-# migrate DB
-echo "Migrating database..."
-docker-compose exec -d web /bin/bash -c "cd /app && rake db:migrate"
+echo "Setting up the database..."
+docker-compose exec -d web /bin/bash -c "cd /app && rake db:setup"
