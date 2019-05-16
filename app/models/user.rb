0f4ae37481a042206085cc2fdaa4799b45e9e78f
Christian Müller <2605803+klumbe@users.noreply.github.com>
2019-05-16 13:19:26 +0200

    TimeEntries will now be deleted if the assoziated User gets deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :time_entries
+  has_many :time_entries, dependent: :destroy
 end
