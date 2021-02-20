75f29333a7c9a7740e5e38ab8e7df0999ece5312
Christian Müller <2605803+klumbe@users.noreply.github.com>
2021-02-20 10:56:17 +0100

    Ran rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'time_entries#index'
 
   resources :time_entries
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
